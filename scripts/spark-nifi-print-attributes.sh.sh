813670b2cda33d8000da2f11f999d43160d33414
jobinthompu <jobinthompu@gmail.com>
2017-10-24 15:46:48 -0400

    Spark+nifi code
    Spark+nifi code

---
+++ scripts/spark-nifi-print-attributes.sh.sh
@@ -0,0 +1,36 @@
+// Import all the libraries required
+import org.apache.nifi._
+import java.nio.charset._
+import org.apache.nifi.spark._
+import org.apache.nifi.remote.client._
+import org.apache.spark._
+import org.apache.nifi.events._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.nifi.remote._
+import org.apache.nifi.remote.client._
+import org.apache.nifi.remote.protocol._
+import org.apache.spark.storage._
+import org.apache.spark.streaming.receiver._
+import java.io._
+import org.apache.spark.serializer._
+
+object SparkNiFiData {
+def main(args: Array[String]) {
+        // Build a Site-to-site client config with NiFi web url and output port name
+val conf = new SiteToSiteClient.Builder().url("http://node1:9090/nifi").url("http://node2:9090/nifi").url("http://node3:9090/nifi").portName("spark").buildConfig()
+        // Set an App Name
+val config = new SparkConf().setAppName("Nifi_Spark_Attributes")
+        // Create a  StreamingContext
+val ssc = new StreamingContext(config, Seconds(5))
+        // Create a DStream using a NiFi receiver so that we can pull data from specified Port
+val lines = ssc.receiverStream(new NiFiReceiver(conf, StorageLevel.MEMORY_ONLY))
+
+        // Extract the 'uuid' attribute
+val text = lines.map(dataPacket => dataPacket.getAttributes.get("uuid"))
+text.print()
+ssc.start()
+ssc.awaitTermination()
+}
+}
+SparkNiFiData.main(Array())
